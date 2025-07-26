007b5f5ebea4b102b88052c0f53a136609e5d5c0
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-07-26 08:06:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
-const ytdlRoute = require('./ytdl');
+const ovldl = require('./func/multidl.js');
 
-app.use(ytdlRoute);
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'affichage', 'acceuil.html'));
+});
+
+app.use('/youtube', (req, res) => {
+  res.sendFile(path.join(__dirname, 'affichage', 'youtube.html'));
+});
+
+app.use('/ovldl', ovldl);
 
 app.listen(PORT, () => {
-    console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+  console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
 });
 
 module.exports = app;
