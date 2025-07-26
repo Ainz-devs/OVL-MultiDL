8b60a5199d2d41340554fe95a12f99046a40c864
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-07-26 09:41:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const ovldl = require('./func/multidl.js');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'affichage', 'acceuil.html'));
 });
 
-app.use('/youtube', (req, res) => {
+app.get('/youtube', (req, res) => {
   res.sendFile(path.join(__dirname, 'affichage', 'youtube.html'));
+});
+
+app.get('/telechargement/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'affichage', 'telechargement.html'));
 });
 
 app.use('/ovldl', ovldl);
